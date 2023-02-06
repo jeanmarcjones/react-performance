6501c01fe052d50fbfc3cec4f651c5d9ee16a0c9
jeanmarcjones <20986524+jeanmarcjones@users.noreply.github.com>
2023-02-06 09:25:58 +0000

    exercise 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,20 +1,13 @@
 // Code splitting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
-// 💣 remove this import
-import Globe from '../globe'
+import * as React from 'react';
 
-// 🐨 use React.lazy to create a Globe component which uses a dynamic import
-// to get the Globe component from the '../globe' module.
+const Globe = React.lazy(() => import('../globe'));
 
 function App() {
-  const [showGlobe, setShowGlobe] = React.useState(false)
+  const [showGlobe, setShowGlobe] = React.useState(false);
 
-  // 🐨 wrap the code below in a <React.Suspense /> component
-  // with a fallback.
-  // 💰 try putting it in a few different places and observe how that
-  // impacts the user experience.
   return (
     <div
       style={{
@@ -26,7 +19,7 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
+      <label style={{ marginBottom: '1rem' }}>
         <input
           type="checkbox"
           checked={showGlobe}
@@ -34,14 +27,13 @@ function App() {
         />
         {' show globe'}
       </label>
-      <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
+      <div style={{ width: 400, height: 400 }}>
+        <React.Suspense fallback={<div>Loading Globe...</div>}>
+          {showGlobe ? <Globe/> : null}
+        </React.Suspense>
       </div>
     </div>
-  )
+  );
 }
-// 🦉 Note that if you're not on the isolated page, then you'll notice that this
-// app actually already has a React.Suspense component higher up in the tree
-// where this component is rendered, so you *could* just rely on that one.
 
-export default App
+export default App;
